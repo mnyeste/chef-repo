ce6de74522766f01ff55ed6d293b4c98222ccd0f
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-06-09 23:09:24 +0200

    Starting James as service
    1) install dependency libc6-i386
    2) symlink to /etc/init.d
    3) start the service

--- cookbooks/apache-james/recipes/default.rb
+++ cookbooks/apache-james/recipes/default.rb
@@ -10,17 +10,27 @@
 james_name="apache-james-3.0-beta4"
 tarball="#{james_name}-app.tar.gz"
 
+package "libc6-i386" do
+  action :install
+end
 
 remote_file "/tmp/#{tarball}" do
   source "http://xenia.sote.hu/ftp/mirrors/www.apache.org/james/apache-james/3.0beta4/#{tarball}"       
   mode "0644"
   action :create_if_missing
+  not_if "test -d /opt/#{james_name}"
 end
 
 execute "tar -C /opt -xzf /tmp/#{tarball}" do
   user "root"
+  not_if "test -d /opt/#{james_name}"
 end
 
 link "/etc/init.d/james" do
- to "/opt/#{james_name}/bin/james" 
+ to "/opt/#{james_name}/bin/james"  
 end 
+
+service "james" do
+  supports :status => true, :restart => true
+  action [ :enable, :start ]
+end
