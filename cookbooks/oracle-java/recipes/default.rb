be6a30985780afad0621117ed40bb5f14749430f
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-06-09 20:29:34 +0200

    Base oracle java recipe

--- cookbooks/oracle-java/recipes/default.rb
+++ cookbooks/oracle-java/recipes/default.rb
@@ -6,3 +6,15 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+apt_repository "webupd8team-java" do
+  uri "http://ppa.launchpad.net/webupd8team/java/ubuntu"
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "EEA14886"
+end
+
+package "oracle-java7-installer" do
+  action :install
+end 
