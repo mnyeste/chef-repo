bf665599812217fce122cdfd6de042777736a5af
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-06-11 22:17:27 +0200

    Finetune attributes

--- cookbooks/apache-james/attributes/default.rb
+++ cookbooks/apache-james/attributes/default.rb
@@ -1,4 +1,2 @@
 default[:james][:version]=     "3.0-beta4"
-default[:james][:home]=        "apache-james-#{default[:james][:version]}"
-default[:james][:tarball]=     "#{default[:james][:home]}-app.tar.gz"
-default[:james][:downloadurl]= "http://xenia.sote.hu/ftp/mirrors/www.apache.org/james/apache-james/#{default[:james][:version].sub("-","")}/#{default[:james][:tarball]}"
+default[:james][:mirror] = "http://xenia.sote.hu/ftp/mirrors/www.apache.org/james/apache-james"
--- cookbooks/apache-james/recipes/default.rb
+++ cookbooks/apache-james/recipes/default.rb
@@ -7,28 +7,31 @@
 # All rights reserved - Do Not Redistribute
 #
 
+james_home = "apache-james-#{node[:james][:version]}"
+james_tarball = "#{james_home}-app.tar.gz"
+
 package "libc6-i386" do
   action :install
 end
 
-remote_file "/tmp/#{node[:james][:tarball]}" do
-  source node[:james][:downloadurl]      
+remote_file "/tmp/#{james_tarball}" do
+  source "#{node[:james][:mirror]}/#{node[:james][:version].sub('-','')}/#{james_tarball}"
   mode "0644"
   action :create_if_missing
-  not_if "test -d /opt/#{node[:james][:home]}"
+  not_if "test -d /opt/#{james_home}"
 end
 
-execute "tar -C /opt -xzf /tmp/#{node[:james][:tarball]}" do
+execute "tar -C /opt -xzf /tmp/#{james_tarball}" do
   user "root"
-  not_if "test -d /opt/#{node[:james][:home]}"
+  not_if "test -d /opt/#{james_home}"
 end
 
 link "/etc/init.d/james" do
- to "/opt/#{node[:james][:home]}/bin/james"  
+ to "/opt/#{james_home}/bin/james"  
 end 
 
 service "james" do
   supports :status => true, :restart => true
   action [ :enable, :start ]
-  priority 80
+  priority 99 
 end
