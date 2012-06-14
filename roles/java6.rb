1b7b97e3be653909c89ac96b08302fb23140e492
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-06-14 21:41:44 +0200

    Dropbox reorganization

--- roles/java6.rb
+++ roles/java6.rb
@@ -12,7 +12,7 @@ default_attributes(
     "jdk" => {
       "6" => {
         "x86_64" => {
-          "url" => "http://dl.dropbox.com/u/73252658/jdk-6u33-linux-x64.bin",
+          "url" => "http://dl.dropbox.com/u/73252658/java/jdk-6u33-linux-x64.bin",
           "checksum" => "978daf43e7fc302220174a16aecaf54d5e9b72f1cd1681f36ab14a2b4bad5fd4"
         }
       }
--- roles/java7.rb
+++ roles/java7.rb
@@ -12,7 +12,7 @@ default_attributes(
     "jdk" => {
       "7" => {
         "x86_64" => {
-          "url" => "http://dl.dropbox.com/u/73252658/jdk-7u5-linux-x64.tar.gz",
+          "url" => "http://dl.dropbox.com/u/73252658/java/jdk-7u5-linux-x64.tar.gz",
           "checksum" => "2a118ce9350d0c0cbaaeef286d04980df664b215d6aaf7bc1d4469abf05711bf"
         }
       }
