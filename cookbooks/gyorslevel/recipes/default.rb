6ade536cc2beffd9276545371cbc1bc6c8f6f1af
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-06-15 11:47:40 +0200

    Delete the default root webapp

--- cookbooks/gyorslevel/recipes/default.rb
+++ cookbooks/gyorslevel/recipes/default.rb
@@ -7,20 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
-
-
+directory "#{node[:jetty][:webapp_dir]}/root" do
+  recursive true
+  action :delete
+end
 
 remote_file "#{node[:jetty][:webapp_dir]}/root.war" do
-  source "https://www.dropbox.com/s/gppqi5m36n1oikf/gyorslevel-1.0-20120615.080024-4.war?dl=1"
+  source "https://www.dropbox.com/s/pcza5mqh69srlw7/gyorslevel-1.0-20120615.085639-7.war?dl=1"
   group node[:jetty][:group]
   owner node[:jetty][:user] 
   mode "0644"
   notifies :restart, "service[jetty]"
 end
-
-#cookbook_file "#{node[:jetty][:context_dir]}/gyorslevel.xml" do
-#  source "gyorslevel.xml" 
-#  group node[:jetty][:group]
-#  owner node[:jetty][:user] 
-#  mode "0644"
-#end
