ae0afc1eb2ee0787e881782f4175cc2770787766
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-06-19 23:04:26 +0200

    Add war checksum

--- cookbooks/gyorslevel/recipes/default.rb
+++ cookbooks/gyorslevel/recipes/default.rb
@@ -9,6 +9,7 @@
 
 remote_file "#{node[:jetty][:webapp_dir]}/root.war" do
   source "https://www.dropbox.com/s/7uanxd6q6ko0sfl/gyorslevel-1.0-20120619.125714-8.war?dl=1"
+  checksum "075a60262b2820d932504dce889e9fd7680f3a6acb6d97e11eb7f6580e4f7caf"
   group node[:jetty][:group]
   owner node[:jetty][:user] 
   mode "0644"
