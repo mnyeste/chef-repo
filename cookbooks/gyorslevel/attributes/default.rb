6892e71ed3b7d6ab444bfcc7aa46dced750e8b3f
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-06-20 22:52:32 +0200

    Extract WAR url to attribute

---
+++ cookbooks/gyorslevel/attributes/default.rb
@@ -0,0 +1,3 @@
+default[:gyorslevel][:url]          = "https://www.dropbox.com/s/3l1lje0bs7yqina/gyorslevel-1.0-20120620.203929-1.war"
+default[:gyorslevel][:checksum]     = "d82828e5ee8e0116d8cb07e652fd0e2b6fc78a5c13601ddff04f47e70f179c85"
+  
