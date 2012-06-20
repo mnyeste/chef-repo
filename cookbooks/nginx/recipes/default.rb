cdbf4e31c1b9cfbec7352b6bd6a3f546de31ddfa
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-06-20 22:50:12 +0200

    Nginx base cookbook with jetty proxy

---
+++ cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: nginx
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "nginx"
+
+template "/etc/nginx/sites-available/default" do
+  source "default-site.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+
+service 'nginx' do
+  supports :status => true, :restart => true, :reload => true
+  action :start
+end
+
+
