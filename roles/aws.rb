7ec15286a4c52838c6c64bc4b1ae5c313d25bc29
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-06-22 15:46:25 +0200

    Webapp update

--- roles/aws.rb
+++ roles/aws.rb
@@ -8,8 +8,8 @@ run_list [
 
 override_attributes(
   "gyorslevel" => {
-    "url" => "https://www.dropbox.com/s/7qg5w57lijm0kho/gyorslevel-1.0-20120621.234858-2.war",
-    "checksum" => "https://www.dropbox.com/s/7qg5w57lijm0kho/gyorslevel-1.0-20120621.234858-2.war"
+    "url" => "https://www.dropbox.com/s/7kjyj1723w0318w/gyorslevel-1.0-20120622.125431-4.war",
+    "checksum" => "684e55852cfef59d3513d0b9e733a2323c42970bddeef556b84c32a31fd3cd03"
    },
   "james" => {
     "domain" => "gyorsmail.no-ip.info"
