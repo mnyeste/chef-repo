6d4a249f6907e85e26baf3238e2b12e6c206cae1
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-06-22 15:52:46 +0200

    New version

--- cookbooks/gyorslevel/attributes/default.rb
+++ cookbooks/gyorslevel/attributes/default.rb
@@ -1,3 +1,3 @@
-default[:gyorslevel][:url]          = "https://www.dropbox.com/s/3l1lje0bs7yqina/gyorslevel-1.0-20120620.203929-1.war"
-default[:gyorslevel][:checksum]     = "d82828e5ee8e0116d8cb07e652fd0e2b6fc78a5c13601ddff04f47e70f179c85"
+default[:gyorslevel][:url]          = "https://www.dropbox.com/s/mxzgp0wpdaeqc6s/gyorslevel-1.0-20120622.134856-5.war"
+default[:gyorslevel][:checksum]     = "b1bb681a5f50d3a0cf8e464a0a15e7cbeced9a14554ab64e95dc8aa22c94efce"
   
