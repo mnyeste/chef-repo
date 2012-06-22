09bdbc316f89d96ffea842bd4621e3bac3b63fcd
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-06-22 02:44:01 +0200

    Custom war for alpha test

--- roles/aws.rb
+++ roles/aws.rb
@@ -5,3 +5,13 @@ run_list [
   "role[standalone]",
   "role[noip]"
 ]
+
+override_attributes(
+  "gyorslevel" => {
+    "url" => "https://www.dropbox.com/s/7qg5w57lijm0kho/gyorslevel-1.0-20120621.234858-2.war",
+    "checksum" => "https://www.dropbox.com/s/7qg5w57lijm0kho/gyorslevel-1.0-20120621.234858-2.war"
+   },
+  "james" => {
+    "domain" => "gyorsmail.no-ip.info"
+  }
+)
