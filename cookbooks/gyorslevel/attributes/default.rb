a3f5f72adbabffbf1c780c00f728af02b27be855
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-06-27 14:15:28 +0200

    New version&layout

--- cookbooks/gyorslevel/attributes/default.rb
+++ cookbooks/gyorslevel/attributes/default.rb
@@ -1,5 +1,6 @@
-default[:gyorslevel][:url]          = "https://www.dropbox.com/s/cx2bz75z2hndhz3/gyorslevel-1.0-20120626.200728-6.war"
-default[:gyorslevel][:checksum]     = "80c4682368041d36fc84945b505b8756dc28f78425df44186d388c246bd00b2c"
+default[:gyorslevel][:url]          = "https://www.dropbox.com/s/2a0328wgcw1fkuf/gyorslevel-1.0-20120627.115850-7.war"
+default[:gyorslevel][:checksum]     = "35d318a1ce33ab925740779fbbe5dea4a81fefe44c395a6c59c195e130c95fff"
 
 default[:gyorslevel][:domain]       = "localhost"
 default[:gyorslevel][:expiration]   = "60000"
+default[:james][:host]   = "localhost"
--- roles/aws.rb
+++ roles/aws.rb
@@ -8,10 +8,9 @@ run_list [
 
 override_attributes(
   "gyorslevel" => {
-    "url" => "https://www.dropbox.com/s/7kjyj1723w0318w/gyorslevel-1.0-20120622.125431-4.war",
-    "checksum" => "684e55852cfef59d3513d0b9e733a2323c42970bddeef556b84c32a31fd3cd03"
-   },
-  "james" => {
-    "domain" => "gyorslevel.no-ip.info"
+    "url" => "https://www.dropbox.com/s/2a0328wgcw1fkuf/gyorslevel-1.0-20120627.115850-7.war",
+    "checksum" => "35d318a1ce33ab925740779fbbe5dea4a81fefe44c395a6c59c195e130c95fff",
+    "domain" => "gyorslevel.no-ip.info",
+    "expiration" => "600000"
   }
 )
