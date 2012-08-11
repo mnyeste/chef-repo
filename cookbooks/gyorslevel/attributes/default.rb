aaf06a760ffe377f77c8e97f2b02dc89e2534806
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-08-11 21:33:22 +0200

    Next app version

--- cookbooks/gyorslevel/attributes/default.rb
+++ cookbooks/gyorslevel/attributes/default.rb
@@ -1,6 +1,8 @@
-default[:gyorslevel][:url]          = "https://www.dropbox.com/s/65a9nxg3olmsdbg/gyorslevel-1.0-20120805.102113-10.war"
-default[:gyorslevel][:checksum]     = "b6f9a0c6efcdb47439b3cc0f461911a6b033375596cd350fb19ea300d6ddaaa9"
+default[:gyorslevel][:url]          = "https://www.dropbox.com/s/j53no4ay09eaobw/gyorslevel-1.0-20120811.191906-11.war"
+default[:gyorslevel][:checksum]     = "d6885b8aaa17efa5d183e059fa3f59f1f2662358fd9037989593d6e80b1f4a14"
 
+
+  
 default[:gyorslevel][:domain]       = "localhost"
 default[:gyorslevel][:expiration]   = "60000"
 default[:gyorslevel][:staticstore]   = "/tmp/gyorslevel/staticstore"
