7e2c5c727c3975fd2c36b99b0249d0b8ca07587c
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-08-18 17:21:54 +0200

    New version

--- roles/vagrant.rb
+++ roles/vagrant.rb
@@ -8,8 +8,8 @@ run_list [
 override_attributes(
   "gyorslevel" => {
     "war" => {
-      "url" => "http://dl.dropbox.com/u/73252658/gyorslevel/gyorslevel-1.0-60251513b9.war",
-      "checksum" =>"7921d345918bd7f66096136ef18f8e066e2a77614ba19dd05d947da5faa80c3c"
+      "url" => "http://dl.dropbox.com/u/73252658/gyorslevel/gyorslevel-1.0-0b7f691532.war",
+      "checksum" =>"9b5a15942f4e9d66879713144333fbf4f618cbb71327f72df7b4b87af89464db"
     }
   }
 )
