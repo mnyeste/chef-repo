2c71ee0e59b938d2ad6cd70b9c8b9aae50588e02
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-08-27 21:36:19 +0200

    New domain name

--- roles/aws.rb
+++ roles/aws.rb
@@ -12,7 +12,7 @@ override_attributes(
       "url" => "http://dl.dropbox.com/u/73252658/gyorslevel/gyorslevel-1.0-c2f47445e2.war",
        "checksum" =>"44075fe700d58809ad53a86720dd04475d9fb7aed7bee07e83749358007b71b2"
         },    
-    "domain" => "gyorslevel.no-ip.info",
+    "domain" => "gyorslevel.info",
     "expiration" => "600000"
   }
 )
