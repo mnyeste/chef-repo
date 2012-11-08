be0d5af9a8b142a4056ede193fd5e7a0a2a56d8c
Mihály Nyeste <mihaly.nyeste@gmail.com>
2012-11-08 21:54:16 +0100

    Dirty patch for multi-lingual mode

--- roles/aws.rb
+++ roles/aws.rb
@@ -9,8 +9,8 @@ run_list [
 override_attributes(
   "gyorslevel" => {
     "war" => {
-      "url" => "http://dl.dropbox.com/u/73252658/gyorslevel/gyorslevel-1.0-db17ae7403.war",
-          "checksum" =>"71dc6af22f25ddfe66b24162fa228f878e12bce7d1180ea67d3b986176b24471"
+      "url" => "http://dl.dropbox.com/u/73252658/gyorslevel/gyorslevel-1.0-ab350299ca.war",
+      "checksum" =>"e6044c6a401ee37336be3faa2f0e4fa81e3ae843831a7e4304f39f2fe2e49121"
      },    
     "domain" => "gyorslevel.info",
     "expiration" => "600000"
